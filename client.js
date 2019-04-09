a950b2eb58c759a780a9122c2087afe3a9668047
Laura Badgley <l.badgleyy@gmail.com>
2019-04-09 14:06:37 -0700

    finished up with comments included

--- client.js
+++ client.js
@@ -7,6 +7,21 @@ const rl = readline.createInterface({
   prompt: '> '
 });
 
+const client = net.createConnection(7890, '192.168.1.238', () => {
+  console.log('i am connected');
+
+  rl.prompt();
+  rl.on('line', line => {
+    client.write(line);
+    rl.prompt();
+  });
+});
+
+client.on('data', data => {
+  console.log(`\n${data.toString()}`);
+  rl.prompt();
+});
+
 
 // const client = net.createConnection(7890, '192.168.1.145', () => {
 //   console.log('I am connected!');
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const net = require('net');
 const server = net.createServer(client => {
   console.log('client connected!');
-  
-  client.on('data', data => {
-    console.log(data.toString());
+
+  const allClients = [];
+
+  const server = net.createServer(connectedClient => {
+    console.log('client connected!');
+    allClients.push(connectedClient);
+    
+  connectedClient.on('data', data => {
+    allClients.forEach(client => {
+      client.write(`ECHO FROM SERVER: ${data}`);
+    });
   });
 });
-
-server.listen(7890);
 // every client is a socket (node js calls it a socket)
 // sockets are when clients request data from you
-
