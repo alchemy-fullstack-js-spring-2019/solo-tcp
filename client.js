db4a0dbf4b08bd4e1847123a584e94ed8e47205a
Cheri Portman <cheriportman@hotmail.com>
2019-04-09 11:28:57 -0700

    client server connected,console log chat

--- client.js
+++ client.js
@@ -1,6 +1,26 @@
 const net = require('net');
+const readline = require('readline');
 
-const client = net.createConnection(9999, 'localHost', () => {
-  console.log('I am connected');
-  client.write('hello I am a client');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '> '
 });
+
+const client = net.createConnection(9990, 'localHost', () => {
+  console.log('I am connected');
+  // client.write('hello I am a client');
+
+  rl.prompt();
+  rl.on('line', line => {
+    client.write(line);
+    rl.prompt();
+  });
+});
+
+
+client.on('data', data => {
+  console.log(`\n${data.toString()}`);
+  rl.prompt();
+});
+
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const net = require('net');
 
-
 //created every time a new person connects to the server aka socket
 const server = net.createServer(client => {
   console.log('client connected!!');
+  
   client.on('data', data => {
-    console.log(data.toString());
+    client.write(`ECHO FROM SERVER: ${data}`);
   });
 });
 
-server.listen(9999);
+server.listen(9990);
