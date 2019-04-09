e72cdcb132ecb13f0ff45e8e872207b6c99ff3b3
Masmics <cherlocker3113@gmail.com>
2019-04-09 12:22:06 -0700

    remove dev notes

--- client.js
+++ client.js
@@ -1,28 +1,20 @@
 const net = require('net');
-const readLine = require('readline'); // MUST npm i -D readline
+const readLine = require('readline');
 
 const rl = readLine.createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: '$ '
 });
-//console.log(rl);
-
-//rl is an event emitter
-//rl.prompt();
 
 const client = net.createConnection(7890, '192.168.1.183', () => {
-  //ryan's '192.168.1.238'
-
   console.log('I am connected');
-  // client.write('Hello I am a client');
   rl.prompt();
   rl.on('line', line => {
     client.write(line);
     rl.prompt();
   });
 });
-
 client.on('data', data => {
   console.log(`\n${data.toString()}`);
   rl.prompt();
--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const net = require('net');
 
-//every client is a socket
 const allClients = [];
 
 const server = net.createServer(connectedClient => {
   console.log('client connected with server listening on 7890');
-  //client.pipe(client);
   allClients.push(connectedClient);
 
   connectedClient.on('data', data => {
     connectedClient.write(`ECHO FROM SERVER: ${data}`);
-    // console.log(data.toString);  
-    // console.log('Client: /`${data}/`');
   });
   connectedClient.end('end', () => {
     console.log('server disconnected');
